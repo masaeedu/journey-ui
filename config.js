06dd1cb4c71eb969c3d6bf2289a9b412ddcf9302
Asad Saeeduddin <masaeedu@gmail.com>
2016-01-23 23:46:21 -0500

    setup

--- config.js
+++ config.js
@@ -1,19 +1,32 @@
 System.config({
-  baseURL: "/",
-  defaultJSExtensions: true,
-  transpiler: "traceur",
-  paths: {
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
-  },
+    baseURL: "/",
+    defaultJSExtensions: true,
+    transpiler: "traceur",
+    typescriptOptions: {
+        tsconfig: true
+    },
+    packages: {
+        "lib": {
+            "defaultExtension": "ts",
+            "meta": {
+                "*.ts": {
+                    "loader": "ts"
+                }
+            }
+        }
+    },
+    paths: {
+        "github:*": "jspm_packages/github/*",
+        "npm:*": "jspm_packages/npm/*"
+    },
 
-  map: {
-    "css": "github:systemjs/plugin-css@0.1.20",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.93",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93",
-    "ts": "github:frankwallis/plugin-typescript@2.5.1",
-    "github:frankwallis/plugin-typescript@2.5.1": {
-      "typescript": "npm:typescript@1.7.5"
+    map: {
+        "css": "github:systemjs/plugin-css@0.1.20",
+        "traceur": "github:jmcriffey/bower-traceur@0.0.93",
+        "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93",
+        "ts": "github:frankwallis/plugin-typescript@2.5.1",
+        "github:frankwallis/plugin-typescript@2.5.1": {
+            "typescript": "npm:typescript@1.7.5"
+        }
     }
-  }
 });
